8ffbd0119d639e5815f8064806d40a9aed2f3e52
Cory Cox <corycox@Corys-MBP.lan>
2023-01-27 15:43:42 -0500

    initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import headshot from "../img/cory-headshot.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <table cellSpacing="20">
+        <tr>
+          <td>
+            <img src={headshot} alt="Cory Cox Headshot" />
+          </td>
+          <td>
+            <h1>Cory Cox</h1>
+            <p>
+              <em>Help Desk Specialist</em>
+            </p>
+            <p>
+              I am a Help Desk Specialist aspiring to be a Software Developer. I
+              have a Bachelor of Science in Computer and Information Technology
+              and 6 years of experience in the Desktop Support area of the
+              Information Technology Field. I enjoy doing DIY projects, cycling
+              and spending time with my Italian Greyhound Nico.
+            </p>
+          </td>
+        </tr>
+      </table>
+      <button>
+        <a href="mailto:cory@corycox.com">Email Me</a>
+      </button>
+      <div className="socialmedia-nav">
+        <ul className="nav">
+          <li>
+            <a href="#">LinkedIn</a>
+          </li>
+          <li>
+            <a href="#">GitHub</a>
+          </li>
+          <li>
+            <a href="#">Instagram</a>
+          </li>
+          <li>
+            <a href="#">Facebook</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
