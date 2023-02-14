30805933a4663194bb2de88469f032a57ada42c9
Cory Cox <corycox@Corys-MBP.lan>
2023-02-14 15:04:45 -0500

    contact form

--- work-in-progress/corycox/src/components/Contact.jsx
+++ work-in-progress/corycox/src/components/Contact.jsx
@@ -5,6 +5,8 @@ function Contact() {
     <div className="contact-cory" id="contact-me">
       <h2 className="contact-header section-head">Contact Cory</h2>
       <form name="contact" method="POST" data-netlify="true" netlify>
+      <input type="hidden" name="form-name" value="contact" />
+
         <p>
           <label>
             Your Name: <input type="text" name="name" />
