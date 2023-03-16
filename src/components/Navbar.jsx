b28bb2a9e68dc457e1f22b96aa970ccd3e8a8d68
Cory Cox <corycox@Corys-MBP.lan>
2023-03-15 21:44:08 -0400

    navbar mobile optimizing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 // importing component items
 
 import NavbarUl from "./NavbarUl";
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className="navbar">
       <h1 className="h1-home">
         <a href="index.html">Cory Cox</a>
       </h1>
-      <nav>
-        <NavbarUl />
+      <nav className="site-nav">
+        <div
+          className={`navbar-toggle ${isActive ? "active" : ""}`}
+          onClick={() => setIsActive(!isActive)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={`navbar-menu ${isActive ? "active" : ""}`}>
+          <NavbarUl />
+        </div>
       </nav>
     </div>
   );
--- work-in-progress/corycox/src/components/NavbarUl.jsx
+++ work-in-progress/corycox/src/components/NavbarUl.jsx
@@ -3,12 +3,24 @@ import NavbarItem from "./NavbarItem";
 
 export default function NavbarUl() {
   return (
-    <ul className="nav nav-header">
-      <NavbarItem navbarURL="#about-cory" navbarText="About Cory" />
-      <NavbarItem navbarURL="#experience-cory" navbarText="Experiences" />
-      <NavbarItem navbarURL="#cory-hobby" navbarText="Hobbies" />
+    <ul className="nav nav-header mobile-nav">
       <NavbarItem
-        navbarClassName="me-3"
+        navbarURL="#about-cory"
+        navbarText="About Cory"
+        navbarClassName="nav-item"
+      />
+      <NavbarItem
+        navbarURL="#experience-cory"
+        navbarText="Experiences"
+        navbarClassName="nav-item"
+      />
+      <NavbarItem
+        navbarURL="#cory-hobby"
+        navbarText="Hobbies"
+        navbarClassName="nav-item"
+      />
+      <NavbarItem
+        navbarClassName="me-3 nav-item"
         navbarURL="#contact-me"
         navbarText="Contact Me"
       />
